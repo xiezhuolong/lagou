ce966067499dad4eedd13f8b5e83055c51509e12
xiezhuolong <358114916@qq.com>
2021-07-26 16:34:11 +0800

    从工作电脑提交，移动package路径

---
+++ 第一阶段模块二作业题/lagou-transfer/src/main/java/com/lagou/edu/frame/start/annotations/Transaction.java
@@ -0,0 +1,10 @@
+package com.lagou.edu.frame.start.annotations;
+
+public class Transaction {
+    //·如何处理代理链
+    //生成代理类后再代理代理类（是用什么模式？proxy、cglib，暂定cglib）
+    //·如何处理事务传递
+    //
+    //2.事务顺序定在最核心部分（不包括AOP）部分
+
+}
